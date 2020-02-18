04c52606a58744cdafe602b8f695df66cce7c715
ujjwal@ujjwal <ujjwal.kumar@tothenew.com>
2020-02-18 19:24:58 +0530

    commit the branch

--- q1.js
+++
@@ -1,5 +0,0 @@
-var amt = prompt("please enter the amount");
-var intrst = prompt("please enter the interest");
-var year = prompt("please enter the year");
-si =(amt*intrst*year)/100;
-alert(`The simple interest is ${si}`);
--- q3.js
+++
@@ -1,9 +0,0 @@
-function areaOfCircle(radius){
-    const pi = 3.14;
-    var area;
-    area = pi * radius * radius;
-return area;
-}
-
-var r = prompt('Enter the radius of the circle');
-alert(`The area is ${areaOfCircle(r)}`);
