3f24585720eabb8eab6c928d21413371fbd86701
ujjwal@ujjwal <ujjwal.kumar@tothenew.com>
2020-03-20 18:11:20 +0530

    committed the Async exercise

--- New_BootCamp/BootCamp/storage2_Exercise/app/src/main/java/com/example/storage2_exercise/ListItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.storage2_exercise;
-
-public interface ListItemClickListener {
-
-
-    public void onDelete(Employee employee, int position);
-
-    public void onEdit(Employee employee, int position);
-}
